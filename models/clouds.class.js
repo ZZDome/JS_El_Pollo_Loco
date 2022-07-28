4614af73c2e24464fd58b54034ffc6ab2a0b0f74
ZZDome <zdome7z@yahoo.de>
2022-07-28 13:21:08 +0200

    cloud speed

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -3,8 +3,17 @@ class Cloud extends MovableObject {
     y = 0;
     height = 300;
     width = 500;
+    speed = Math.random();
+    despawnX = 0 - this.width;
 
     constructor(){
         super().loadImage('../img/5_background/layers/4_clouds/1.png')
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+            this.x -= 0.20 * this.speed;
+        }, 1000 / 60);
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,15 +1,18 @@
 class World{
     air = new Air;
-    bglayer3 = new Backgroundlayer3;
-    bglayer2 = new Backgroundlayer2;
-    bglayer1 = new Backgroundlayer1;
+    bglayer3 = [new Backgroundlayer3()];
+    bglayer2 = [new Backgroundlayer2()];
+    bglayer1 = [new Backgroundlayer1()];
     character = new Character;
     enemies = [
         new Chicken(),
         new Chicken(),
-        new Chicken(),
+        new Chicken()
     ];
-    cloud = new Cloud;
+    cloud = [
+        new Cloud(),
+        new Cloud(),
+    ];
     canvas;
     ctx;
 
@@ -20,17 +23,26 @@ class World{
         this.draw();
     }
 
-    draw(){
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-        this.ctx.drawImage(this.air.img, this.air.x, this.air.y, this.air.width, this.air.height);
-        this.ctx.drawImage(this.bglayer3.img, this.bglayer3.x, this.bglayer3.y, this.bglayer3.width, this.bglayer3.height);
-        this.ctx.drawImage(this.bglayer2.img, this.bglayer2.x, this.bglayer2.y, this.bglayer2.width, this.bglayer2.height);
-        this.ctx.drawImage(this.bglayer1.img, this.bglayer1.x, this.bglayer1.y, this.bglayer1.width, this.bglayer1.height);
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+    drawObjects(objects){
+        objects.forEach(objects => {
+            this.ctx.drawImage(objects.img, objects.x, objects.y, objects.width, objects.height);
+
         });
-        this.ctx.drawImage(this.cloud.img, this.cloud.x, this.cloud.y, this.cloud.width, this.cloud.height);
+    }
+
+    drawObject(object){
+        this.ctx.drawImage(object.img, object.x, object.y, object.width, object.height);
+    }
+
+    draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawObject(this.air)
+        this.drawObjects(this.bglayer3)
+        this.drawObjects(this.bglayer2)
+        this.drawObjects(this.bglayer1)
+        this.drawObjects(this.cloud)
+        this.drawObjects(this.enemies)
+        this.drawObject(this.character)        
         
         let self = this;
         requestAnimationFrame(function() {
