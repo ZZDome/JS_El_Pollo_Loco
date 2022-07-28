b27be7aee2a360fba49cde1112198d0ccc477962
ZZDome <zdome7z@yahoo.de>
2022-07-28 14:47:52 +0200

    animations

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,52 @@
 class Character extends MovableObject {
-    
+    x = 30;
+    y = 175;
+    height = 250;
+    width = 100;
+    currentImage = 0;
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ];
+
+    IMAGES_IDLE = [
+        'img/2_character_pepe/1_idle/idle/I-1.png',
+        'img/2_character_pepe/1_idle/idle/I-2.png',
+        'img/2_character_pepe/1_idle/idle/I-3.png',
+        'img/2_character_pepe/1_idle/idle/I-4.png',
+        'img/2_character_pepe/1_idle/idle/I-5.png',
+        'img/2_character_pepe/1_idle/idle/I-6.png',
+        'img/2_character_pepe/1_idle/idle/I-7.png',
+        'img/2_character_pepe/1_idle/idle/I-8.png',
+        'img/2_character_pepe/1_idle/idle/I-9.png',
+        'img/2_character_pepe/1_idle/idle/I-10.png'
+    ];
 
     constructor(){
-        super().loadImage('../img/2_character_pepe/2_walk/W-21.png')
+        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_IDLE);
+        this.animateIdle();
+    }
+
+    animateWalk(){
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
+    }
+
+    animateIdle(){
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_IDLE.length;
+            let path = this.IMAGES_IDLE[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
     }
 }
--- models/movableObject.class.js
+++ models/movableObject.class.js
@@ -4,9 +4,19 @@ class MovableObject{
     width = 150;
     height = 300;
     img;
+    imageCache = {};
 
     loadImage(path){
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(arr){
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+
     }
 }
